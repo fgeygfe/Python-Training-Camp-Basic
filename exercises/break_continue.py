f5bfe0bda31928b84d70ce0a79be6bfdb109c46b
Jue Wang <693117188@qq.com>
2025-05-23 18:28:20 +0800

    Update break_continue.py

--- exercises/break_continue.py
+++ exercises/break_continue.py
@@ -21,6 +21,6 @@ def skip_multiples_of_three(n):
     pass 
     result=[]
     for num in range(1,n+1):
-        if(num%3==0) continue
+        if(num%3==0): continue
         result.append(num)
     return result
